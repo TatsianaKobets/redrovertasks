81461ea2edbc070a1e580ca5a071658bda3d0d30
kobets.tatsiana <kobets.tatsiana@gmail.com>
2024-09-07 21:08:38 +0300

    Homework Task5

--- src/main/java/by/http/redrovertasks/Task5_2.java
+++ src/main/java/by/http/redrovertasks/Task5_2.java
@@ -1,8 +1,93 @@
 package by.http.redrovertasks;
 
+import java.util.Arrays;
+
 public class Task5_2 {
 
   public static void main(String[] args) {
-  }
+    String[][] arrayStrings = {{"Привет", "всем", "кто"}, {"изучает", "язык", "программирования"},
+        {"java"}};
 
+    /**Задача №1
+
+     Дана строка:
+     String s = “Перестановочный алгоритм быстрого действия”;
+     необходимо вывести все буквы “о” из этой строки.
+     Для указанной строки ответ будет “ооооо” (или в столбик)
+     */{
+      System.out.println("Задача 5.2 - 1");
+      String s = "Перестановочный алгоритм быстрого действия";
+      for (int i = 0; i < s.length(); i++) {
+        if (s.charAt(i) == 'о') {
+          System.out.println("о");
+        }
+      }
+    }
+
+    /** Задача №2
+     Дана строка:
+     String s = “Перевыборы выбранного президента”;
+     необходимо подсчитать количество букв “е” в строке.
+     Для указанной строки ответ будет 4.
+     */
+    {
+      System.out.println("Задача 5.2 - 2");
+      String s = "Перевыборы выбранного президента";
+      int count = 0;
+      for (char c : s.toCharArray()) {
+        if (c == 'е') {
+          count++;
+        }
+      }
+      System.out.println("Количество букв 'е': " + count);
+    }
+    /** Экстра задача №1
+     Дана строка:
+     String s = “Посмотрите как Рите нравится ритм”;
+     необходимо вывести индексы начала всех подстрок - “рит”, независимо от регистра.
+     Для указанной строки ответ будет 6, 15, 29.
+     */
+    {
+      System.out.println("Задача 5.2 - *1");
+      String s = "Посмотрите как Рите нравится ритм";
+      String sub = "рит";
+      int index = s.toLowerCase().indexOf(sub.toLowerCase());
+      while (index != -1) {
+        System.out.println(index);
+        index = s.toLowerCase().indexOf(sub.toLowerCase(), index + 1);
+      }
+    }
+    /**Экстра задача №2
+     Дан массив:
+     String[][] array = {{“Привет”, “всем”, “кто”}, {“изучает”, “язык”, “программирования”}, {“java”}};
+     необходимо подсчитать количество строк в массиве, которые не содержат буквы “е”.
+     */
+    {
+      System.out.println("Задача 5.2 - *2");
+      int count = 0;
+      for (String[] row : arrayStrings) {
+        boolean containsE = false;
+        for (String str : row) {
+          if (str.toLowerCase().contains("е")) {
+            containsE = true;
+            break;
+          }
+        }
+        if (!containsE) {
+          count++;
+        }
+      }
+      System.out.println("Количество строк без буквы 'е': " + count);
+    }
+    {
+      System.out.println("Задача 5.2 - *3 V-2.0");
+      int count = 0;
+      for (String[] row : arrayStrings) {
+        if (!Arrays.stream(row).anyMatch(str -> str.toLowerCase().contains("е"))) {
+          count++;
+        }
+      }
+      System.out.println("Количество строк без буквы 'е': " + count);
+    }
+  }
 }
---
+++ src/main/java/by/http/redrovertasks/Task5_3.java
@@ -0,0 +1,85 @@
+package by.http.redrovertasks;
+
+public class Task5_3 {
+
+  public static void main(String[] args) {
+    //Вывести следующие строки с соответствующим форматированием (как пирамиды):
+
+    /** Задача №1
+     0  1  2  3  4  5  6  7  8  9
+     0  1  2  3  4  5  6  7  8
+     0  1  2  3  4  5  6  7
+     0  1  2  3  4  5  6
+     0  1  2  3  4  5
+     0  1  2  3  4
+     0  1  2  3
+     0  1  2
+     0  1
+     0
+     */
+    {
+      System.out.println("Задача 5.3_1");
+      for (int i = 10; i > 0; i--) {
+        for (int j = 0; j < i; j++) {
+          System.out.print(j + "  ");
+        }
+        System.out.println();
+      }
+    }
+
+    /** Задача №2
+
+     0  1  2  3  4  5  6  7  8  9
+     0  1  2  3  4  5  6  7  8
+     0  1  2  3  4  5  6  7
+     0  1  2  3  4  5  6
+     0  1  2  3  4  5
+     0  1  2  3  4
+     0  1  2  3
+     0  1  2
+     0  1
+     0
+
+     */
+    {
+      System.out.println("Задача 5.3_2");
+      for (int i = 10; i > 0; i--) {
+        for (int j = 10 - i; j > 0; j--) {
+          System.out.print(" ");
+        }
+        for (int j = 0; j < i; j++) {
+          System.out.print(j + "  ");
+        }
+        System.out.println();
+      }
+    }
+    /**  Задача №3
+
+     9 8 7 6 5 4 3 2 1 0 1 2 3 4 5 6 7 8 9
+       8 7 6 5 4 3 2 1 0 1 2 3 4 5 6 7 8
+         7 6 5 4 3 2 1 0 1 2 3 4 5 6 7
+           6 5 4 3 2 1 0 1 2 3 4 5 6
+             5 4 3 2 1 0 1 2 3 4 5
+               4 3 2 1 0 1 2 3 4
+                 3 2 1 0 1 2 3
+                   2 1 0 1 2
+                     1 0 1
+                       0
+     */
+    {
+      for (int i = 9; i >= 0; i--) {
+        for (int j = 9 - i; j > 0; j--) {
+          System.out.print(" ");
+        }
+        for (int j = i; j >= 0; j--) {
+          System.out.print(j + " ");
+        }
+        for (int j = 1; j <= i; j++) {
+          System.out.print(j + " ");
+        }
+        System.out.println();
+      }
+    }
+  }
+
+}
